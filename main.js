c3e4945bb7c7a1cee9dcf74c01809467f7e7ff33
Cuozzo, Giovanni <cuozzogiovanni03@gmail.com>
2021-10-15 14:35:56 +0200

    test

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import * as THREE from "./node_modules/three/build/three.module.js";
-
+//testing stuffs
 // Initial stuff
 const scene = new THREE.Scene();
 const loader = new THREE.TextureLoader();
