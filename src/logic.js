fe6a02d06a30e0bd2dc8e391d3057014fc29aada
Cuozzo, Giovanni <cuozzogiovanni03@gmail.com>
2021-10-19 12:49:39 +0200

    Deprecated managezoom, added OrbitControls

--- src/logic.js
+++ src/logic.js
@@ -1,14 +1,24 @@
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import gsap from "gsap";
 
 // Initial stuff
 const scene = new THREE.Scene();
 const loader = new THREE.TextureLoader();
-const camera = new THREE.PerspectiveCamera(55, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.z = 1.5;
-
+const camera = new THREE.PerspectiveCamera(
+  55,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+const controls = new OrbitControls( camera, renderer.domElement );
+camera.position.z = 1.5;
+controls.update();
+
+
 
 // Stars background
 const bgTexture = loader.load("images/galaxy_starfield.png");
@@ -36,31 +46,44 @@ var globe = new THREE.Mesh(
 
 // Add clouds
 var clouds = new THREE.Mesh(
-  new THREE.SphereGeometry(0.503, 32, 32),
+  new THREE.SphereGeometry(0.510, 32, 32),
   new THREE.MeshPhongMaterial({
     map: loader.load("images/fair_clouds_4k.png"),
     transparent: true,
   })
 );
 
+let mesh = new THREE.Mesh(
+  new THREE.SphereBufferGeometry(0.1, 20, 20),
+  new THREE.MeshBasicMaterial({ color: 0xff0000 })
+);
+mesh.position.set(1, 0, 0);
+scene.add(mesh);
+
 // Add everything to scene
 scene.add(light, globe, clouds);
 
-// Manage zoom
-document.addEventListener("mousewheel", (event) => {
-  event.preventDefault();
-  camera.position.z < 1 ? (camera.position.z = 1) : "";
-  camera.position.z > 2.5 ? (camera.position.z = 2.25) : "";
-  if (camera.position.z >= 1 && camera.position.z <= 2.5) {
-    camera.position.z += event.deltaY / 500;
-  }
-});
+// // DEPRECATED
+// // Manage zoom 
+// document.addEventListener("mousewheel", (event) => {
+//   event.preventDefault();
+//   camera.position.z < 1 ? (camera.position.z = 1) : "";
+//   camera.position.z > 2.5 ? (camera.position.z = 2.25) : "";
+//   if (camera.position.z >= 1 && camera.position.z <= 2.5) {
+//     camera.position.z += event.deltaY / 500;
+//   }
+// });
+
+
+// edits 2.0
+
 
 // Render
 render();
 function render() {
+  requestAnimationFrame(render);
   globe.rotation.y += 0.0005;
   clouds.rotation.y += 0.0005;
-  requestAnimationFrame(render);
+  controls.update();
   renderer.render(scene, camera);
 }
