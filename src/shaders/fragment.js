253ad12c546731a8b940c53ddc32359a9a026f08
Cuozzo, Giovanni <cuozzogiovanni03@gmail.com>
2021-11-03 12:28:32 +0100

    Outline working / clean code / popup onclick

--- src/shaders/fragment.js
+++ src/shaders/fragment.js
@@ -5,14 +5,25 @@
 // `;
 
 const fragmentShader = `
-uniform sampler2D tMatCap;
+uniform sampler2D mapIndex;
+uniform sampler2D lookup;
+uniform sampler2D outline;
+uniform sampler2D blendImage;
+varying vec3 vNormal;
+varying vec2 vUv;
+void main() 
+{
+	vec4 mapColor = texture2D( mapIndex, vUv );    
+	float indexedColor = mapColor.y;       
+	vec2 lookupUV = vec2( indexedColor, 0.0 );
+	vec4 lookupColor = texture2D( lookup, lookupUV );                              
+	vec4 outlineColor = texture2D( outline, vUv );
+	vec4 blendColor = texture2D( blendImage, vUv );
+	// if (outlineColor.x > 0.1) outlineColor = vec4(1.0,1.0,1.0,1.0);
 
-      varying vec2 vN;
-
-      void main() {
-      	vec3 base = texture2D( tMatCap, vN ).rgb;
-      	gl_FragColor = vec4( base, 1. );
-      }
+	
+	gl_FragColor = 0.5 * outlineColor + 1.0 * lookupColor + 0.5 * blendColor;
+}
 `;
 
 export default fragmentShader;
--- src/shaders/vertex.js
+++ src/shaders/vertex.js
@@ -5,20 +5,14 @@
 // `;
 
 const vertexShader = `
-varying vec2 vN;
-
-      void main() {
-      	vec4 p = vec4( position, 1. );
-
-      	vec3 e = normalize( vec3( modelViewMatrix * p ) );
-      	vec3 n = normalize( normalMatrix * normal );
-
-      	vec3 r = reflect( e, n );
-      	float m = 2. * length( vec3( r.xy, r.z + 1. ) );
-      	vN = r.xy / m + .5;
-
-      	gl_Position = projectionMatrix * modelViewMatrix * p;
-      }
+varying vec3 vNormal;
+varying vec2 vUv;
+void main()
+{
+	gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0);
+	vNormal = normalize( normalMatrix * normal );
+	vUv = uv;
+}
 `;
 
 export default vertexShader;    
